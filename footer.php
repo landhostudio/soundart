9f6ab9f7f3d001c4f9120be831698e93a583ec39
h0r0man <roman.horokhovatskyy@gmail.com>
2017-03-08 13:37:28 +0100

    Added container for footer

--- footer.php
+++ footer.php
@@ -1,21 +1,24 @@
     <footer role="contentinfo" class="footer">
-      <h4>
-        <a rel="home" href="<?php echo esc_url(home_url('/')); ?>"><?php bloginfo('name'); ?></span></a>
-      </h4>
-
-      <?php if (has_nav_menu('menu_footer')): ?>
-        <nav role="navigation">
-          <?php wp_nav_menu(array(
-            'theme_location' => 'menu_footer',
-            'items_wrap' => '%3$s'
-          )); ?>
-        </nav>
-      <?php endif; ?>
-
       <div class="container">
-        <p>&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?> – <?php bloginfo('description'); ?>.</p>
-      </div>
 
+        <h4>
+          <a rel="home" href="<?php echo esc_url(home_url('/')); ?>"><?php bloginfo('name'); ?></span></a>
+        </h4>
+
+        <?php if (has_nav_menu('menu_footer')): ?>
+          <nav role="navigation">
+            <?php wp_nav_menu(array(
+              'theme_location' => 'menu_footer',
+              'items_wrap' => '%3$s'
+            )); ?>
+          </nav>
+        <?php endif; ?>
+
+          <div class="copy">
+            <p>&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?> – <?php bloginfo('description'); ?>.</p>
+          </div>
+
+        </div>
     </footer>
 
     <?php wp_footer(); ?>
